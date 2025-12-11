f15e3b0121737ed02dd69e9bd83531557a4f8694
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2025-12-11 13:12:28 +0100

    Built remove_task, add_task, and main functions

--- to-do list.py
+++ to-do list.py
@@ -1,20 +1,36 @@
 # I need to make a while loop that prints the following commands:
-# a) add item
-# b) remove item
+# a) add task
+# b) remove task
 # c) view list 
 
-def prompt():
+def main():
+    print("a) Add task")
+    print("b) Remove task")
+    print("c) View list")
+    
     while True:
-        print("a) Add item")
-        print("b) Remove item")
-        print("c) View list")
+        
         ans = input("Enter command: ")
         ans = ans.lower()
 
-def add_item(item: string);
+        if ans == "a":
+            task_to_add = input("Task to add: ")
+            add_task(task_to_add)
+        elif ans == "b":
+            task_to_remove = input("Task to remove: ")
+            remove_task(task_to_remove)
+        elif ans == "c":
+            print(to_do_list)
+
+
+
+def add_task(task):
+    return to_do_list.append(task)
+    
+def remove_task(task):
+    return to_do_list.remove(task)
     
             
 
-
 to_do_list = []
-prompt()
+main()
