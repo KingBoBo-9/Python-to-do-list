3e3d2f8e38bce18061bd7c26038e24ccfbc054a0
KingBoBo-9 <246374481+KingBoBo-9@users.noreply.github.com>
2025-12-11 12:59:15 +0100

    Writing prompt and add_item function

---
+++ to-do list.py
@@ -0,0 +1,20 @@
+# I need to make a while loop that prints the following commands:
+# a) add item
+# b) remove item
+# c) view list 
+
+def prompt():
+    while True:
+        print("a) Add item")
+        print("b) Remove item")
+        print("c) View list")
+        ans = input("Enter command: ")
+        ans = ans.lower()
+
+def add_item(item: string);
+    
+            
+
+
+to_do_list = []
+prompt()
